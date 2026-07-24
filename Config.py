b1c617ec3d14558a17c81868b9ceeab85f2c5cb9
Test <kalyank9598@gmail.com>
2026-07-24 09:31:35 +0530

    Commiting the project

---
+++ Config.py
@@ -0,0 +1,9 @@
+email = "testplaywright@abc.com"
+password = "Test@123"
+
+invalid_email = "testplaywright@xyz.com"
+invalid_password = "Test@123xyz"
+
+product_name = "MacBook"
+product_quantity = "1"
+total_price = "$602.00"
